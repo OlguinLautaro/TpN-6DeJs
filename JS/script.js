5a667299c87cd1b580afa7cdc198c38c3c5872cc
lautaro <lautaroolguin89@gmail.com>
2022-10-06 19:53:10 -0300

    colocando un let de mostrar multiplicacion

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,6 @@
 const semiDios = ["kratos mato a zeus", "kratos mato a poseidon" , "kratos mato a hercules", "kratos mato a hermes", "kratos mato a hares"]
 var mostsuma = sumar();
 let mostdiv = dividir();
-
+let mostmult = multiplicar();
 
 //#endregion
