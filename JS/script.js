e5e6701e0fc81780ce4e5693943e1ac74e430e21
lautaro <lautaroolguin89@gmail.com>
2022-10-06 19:50:38 -0300

    colocando un var de mostrar suma

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,7 @@
 //#region colocando variables 
 const semiDios = ["kratos mato a zeus", "kratos mato a poseidon" , "kratos mato a hercules", "kratos mato a hermes", "kratos mato a hares"]
+var mostsuma = sumar();
+
+
 
 //#endregion
